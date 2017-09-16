5c5be14a52ce6eaa40f302a36d43111c816e3fca
Luis Magno <luismagno@hotmail.com>
2017-09-16 14:27:00 -0300

    teste verificacao google

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- veriricacao do google-->
+    <meta name="google-site-verification" content="EU1XOkNplctxtQmu1990NQGzBHxilkPE7G7nn9XEhOk" />
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
